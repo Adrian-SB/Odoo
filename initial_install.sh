a27096ad6c21126c012c0a85fbb70d80d40ddcf1
Adrián Silva <asboceta@gmail.com>
2020-05-19 12:53:22 +0200

    initial install

---
+++ initial_install.sh
@@ -0,0 +1,22 @@
+#! \bin\bash
+
+user=$(whoami)
+
+if [ $user = "root" ]
+then
+	#Install Ansible
+	apt -y update
+	apt -y upgrade
+	apt -y install software-properties-common
+	apt-add-repository --yes --update ppa:ansible/ansible
+	apt -y install ansible
+	#Install jenkins
+	apt -y install openjdk-8-jdk
+	apt -y install nginx
+	wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -
+	sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+	apt -y update
+	apt -y install jenkins
+else
+	echo "Debes de ser root para poder ejecutar el script"
+fi
