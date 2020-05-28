807bbf1b8e8a10ba72869a08a03e97a3760f9230
Adrián Silva <asboceta@gmail.com>
2020-05-28 14:43:58 +0200

    ok

--- initial_install.sh
+++ initial_install.sh
@@ -23,7 +23,7 @@ then
 	cat /var/lib/jenkins/secrets/initialAdminPassword
 	echo "-----------------------"
 	#echo "192.168.100.10 jenkins" >> /etc/hosts
-	echo "1" > /proc/sys/net/ipv4/ip_forward
+	#echo "1" > /proc/sys/net/ipv4/ip_forward
 else
 	echo "Debes de ser root para poder ejecutar el script"
 fi
